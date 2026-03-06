fixing property computation + GAN texturizer wip

mt_DIDC_dataset_preprocessing.py
@@ -5,8 +5,10 @@ from datasets import LazyDatasetDIDC
 from tqdm import tqdm
 import numpy as np
 import torch
+import torchvision.transforms.functional as TF
 
-def create_mmap_dataset(source_dir, dest_dir):
+
+def create_mmap_dataset(source_dir, dest_dir, save_images=False):
     os.makedirs(dest_dir, exist_ok=True)
     
     dataset_params = {
@@ -18,7 +20,8 @@ def create_mmap_dataset(source_dir, dest_dir):
         "min_blob_size": None,
         "num_fg_classes_preprocessing": 4,
         "grouping_rules_used": GROUPING_RULES,
-        "new_labels_used": NEW_LABELS
+        "new_labels_used": NEW_LABELS,
+        "save_images": save_images
     }
     
     config_path = os.path.join(dest_dir, "dataset_config.json")
@@ -52,9 +55,14 @@ def create_mmap_dataset(source_dir, dest_dir):
 
         fg_list = []    
         mask_list = []
+        img_list = []
         for idx in valid_indices:
             fg_slice = torch.from_numpy(pat['mask_foreground'][:,:,idx]).unsqueeze(0).long()
             fg_slice[(fg_slice > 3) | (fg_slice < 0)] = 0 # remove noisy labels in the foreground mask
+
+            if save_images:
+                img_slice = torch.from_numpy(pat['interpolated_intensity'][:,:,idx])
+                img_slice = TF.resize(img_slice.unsqueeze(0), dataset_params["target_size_preprocessing"], interpolation=TF.InterpolationMode.BILINEAR).squeeze(0)
 
             mask_vol = pat['interpolated_segmentation']
             if mask_vol.ndim == 1:
@@ -70,21 +78,28 @@ def create_mmap_dataset(source_dir, dest_dir):
                 mask_proc = mask_proc.squeeze(0)
 
             fg_list.append(fg_proc)
-            mask_list.append(mask_proc)        
-
-
+            mask_list.append(mask_proc)
+            if save_images:
+                img_list.append(img_slice)
 
         fg_stack = torch.stack(fg_list, dim=0)
         mask_stack = torch.stack(mask_list, dim=0)
 
         np.save(os.path.join(dest_dir, f"{pat_id}_fg.npy"), fg_stack.numpy())
         np.save(os.path.join(dest_dir, f"{pat_id}_mask.npy"), mask_stack.numpy())
+        if save_images:
+            img_stack = torch.stack(img_list, dim=0)
+            np.save(os.path.join(dest_dir, f"{pat_id}_img.npy"), img_stack.numpy())
                 
-         
+        # msg = f"Processed {file}: fg shape {fg_stack.shape}, mask shape {mask_stack.shape}"
+        # if save_images:
+        #     msg += f", img shape {img_stack.shape}"
+        # print(msg)
 
 
 if __name__ == "__main__":
     source_dir = 'DIDC_multiclass_coro_v2'
     dest_dir = 'DIDC_multiclass_coro_v2_prep_2'
-    create_mmap_dataset(source_dir, dest_dir)
+    save_images = True
+    create_mmap_dataset(source_dir, dest_dir, save_images)
     

tx_compute_properties.py
@@ -15,7 +15,7 @@ import json
 
 from mt_DIDC_config import GROUPING_RULES, PROPERTY_KEY, LABEL2LABEL
 
-from utils import load_original_labels, setup_logger
+from utils import load_original_labels, setup_logger, set_reproducibility
 
 
 @dataclass
@@ -34,6 +34,8 @@ class Config:
     upsample_factor: int = 1
 
     save_hd_images: bool = False
+
+    seed: int = 187
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -183,8 +185,7 @@ class PropertyGenerator:
         h, w, slices = mri_hd.shape
         tissue_props = np.zeros((3, h, w, slices), dtype=np.float32)
         
-        if not os.path.exists(os.path.join(out_path, 'images')):
-            os.makedirs(os.path.join(out_path, f'images/{self.pat_id}/'), exist_ok=True)
+        os.makedirs(os.path.join(out_path, f'images/{self.pat_id}/'), exist_ok=True)
 
         avg_pat_loss = 0.0
         for s in range(slices):
@@ -200,7 +201,7 @@ class PropertyGenerator:
                 ax[0].set_title("Real MRI")
                 ax[1].imshow(pred_img, cmap='gray')
                 ax[1].set_title("Simulated bSSFP")
-                plt.savefig(os.path.join(out_path, f'images/{self.pat_id}/_MRI_{s:05d}.png'), dpi=150)
+                plt.savefig(os.path.join(out_path, f'images/{self.pat_id}/MRI_{s:05d}.png'), dpi=150)
                 plt.close()
         
         avg_pat_loss /= slices
@@ -214,9 +215,10 @@ class PropertyGenerator:
 def main():
     config = Config()
 
+    set_reproducibility(config.seed)
     os.makedirs(config.out_dir, exist_ok=True)
     setup_logger(config.out_dir)
-
+    
     with open(os.path.join(config.out_dir, 'config.json'), 'w') as f:
         json.dump(asdict(config), f, indent=4)
 